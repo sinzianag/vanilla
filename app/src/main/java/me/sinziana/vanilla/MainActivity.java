9bee92f340ab5855596250108646cb776d9f2e8f
Sinziana Gafitanu <sinziana.gafitanu@gmail.com>
2017-01-17 22:52:47 -0800

    Adding search in the Toolbar

--- app/src/main/java/me/sinziana/vanilla/MainActivity.java
+++ app/src/main/java/me/sinziana/vanilla/MainActivity.java
@@ -1,8 +1,12 @@
 package me.sinziana.vanilla;
 
 import android.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         if (findViewById(R.id.fragment_container) != null) {
 
@@ -41,6 +48,20 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // TODO Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
     private void handleBackStack() {
         FragmentManager fragmentManager = getFragmentManager();
