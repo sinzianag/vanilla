9a82aaee293dd591f9a20d3309120cc4c872a4d3
Sinziana Gafitanu <sinziana.gafitanu@gmail.com>
2017-02-27 23:54:25 -0800

    Removing some unused imports

--- app/src/main/java/me/sinziana/vanilla/screens/PunCategories.java
+++ app/src/main/java/me/sinziana/vanilla/screens/PunCategories.java
@@ -29,14 +29,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,7 +42,6 @@ import java.util.List;
 
 import me.sinziana.vanilla.PunCategoryList;
 import me.sinziana.vanilla.R;
-import me.sinziana.vanilla.SearchActivity;
 
 public class PunCategories extends Fragment{
 
